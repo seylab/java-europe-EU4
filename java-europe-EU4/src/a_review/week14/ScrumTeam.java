00cc1743b2175af8c27b04309ef2eb493b37f478
seylab <sweetly35@gmail.com>
2020-12-19 20:01:30 +0300

    week14

--- java-europe-EU4/src/a_review/week14/ScrumTeam.java
+++ java-europe-EU4/src/a_review/week14/ScrumTeam.java
@@ -43,6 +43,19 @@ public class ScrumTeam {
 
     }
 
+    public void terminateTester(String ID){
+        //  testers.removeIf(p -> p.ID.equals(ID) );
+
+        Tester tester = null;
+        for (Tester each: testers){
+            if (each.ID.equals(ID)){
+                tester = each;
+            }
+        }
+        testers.remove(tester);
+
+    }
+
 
 
 
