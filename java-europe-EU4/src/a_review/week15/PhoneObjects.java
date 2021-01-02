0b5065bd6117ce80cd51743b50c9c139390d9e40
seylab <sweetly35@gmail.com>
2021-01-02 20:31:10 +0300

    week15 Review

--- java-europe-EU4/src/a_review/week15/PhoneObjects.java
+++ java-europe-EU4/src/a_review/week15/PhoneObjects.java
@@ -11,6 +11,21 @@ public class PhoneObjects {
         phone1.faceTiming(12312312);
         ((IPhone)phone2).faceTiming(123456785);
         ((IPhone)phone4).faceTiming(213546789);
+        System.out.println();
+
+        phone1.download();
+        ((IPhone)phone2).download();
+        ((AppleApp)phone2).download();
+        phone4.download();
+        System.out.println();
+
+        phone1.calling(2314556);
+        phone2.calling(2123564);
+        ((Phone)phone3).calling(2565458);
+        ((IPhone)phone3).calling(36598565);
+        ((IPhone) phone4).calling(21154565);
+
+        phone1.calling(3231);
 
 
     }
