0aeede261ebdd5ddea1329894ae5eb9bdbc05860
seylab <sweetly35@gmail.com>
2021-01-06 02:11:28 +0300

    day_46_collection_part_1

---
+++ java-europe-EU4/src/day_46_collection_part_1/ArrayVSArratList.java
@@ -0,0 +1,30 @@
+package day_46_collection_part_1;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ArrayVSArratList {
+
+    public static void main(String[] args) {
+
+        int[] numsArray = {30, 50, 100, 5, 6};
+        String[] days = new String[7];
+        days[0] = "Monday";
+
+        Object[] obj = {2, "apple", false};
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        Collection<Integer> list3 = new ArrayList<>();
+
+        list1.add(44);
+        list1.add(50);
+        list1.add(100);
+
+        System.out.println(numsArray[0]);
+        System.out.println(list1.get(0));
+
+
+    }
+}
